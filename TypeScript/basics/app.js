ac828a564882194c8698e1e3ff8590e9fff4a9a9
owaisk23 <owais1566@gmail.com>
2025-05-02 18:48:11 +0500

    added alias type

--- TypeScript/basics/app.js
+++ TypeScript/basics/app.js
@@ -1,30 +1,76 @@
 console.log("Welcome in Typescript");
-var userName = "Owais Ahmed";
-var userAge = 23;
-var userEmail = "owais@gmail.com";
-var isActive = true;
-console.log("Hello ".concat(userName));
-var userDetails = "User Name: ".concat(userName, ", User Age: ").concat(userAge, ", User Email: ").concat(userEmail, ", Is Active: ").concat(isActive);
+// let userName: string = "Owais Ahmed";
+// let userAge: number = 23;
+// let userEmail: string = "owais@gmail.com"
+// let isActive: boolean = true;
+// console.log(`Hello ${userName}`);
+// let userDetails = `User Name: ${userName}, User Age: ${userAge}, User Email: ${userEmail}, Is Active: ${isActive}`;
+// console.log(userDetails);
+// // null==false  true=1 false=0
+// let userId = null;
+// if(userId){
+//     console.log("User ID is true");
+// }else{
+//     console.log("User ID is false");
+// }
+// // undefined==false  true=1 false=0
+// let userPost = undefined;
+// if(userPost){
+//     console.log("userPost is true");
+// }else{
+//     console.log("userPost is false");
+// }
+// // Array
+// let userMarks: number[] = [45, 60, 80, 90];
+// // userMarks.push(100); //end 100
+// // userMarks.pop(); //end delete last element
+// // userMarks.shift(); //start delete
+// console.log(userMarks);
+// // forEach loop
+// userMarks.forEach((val)=>{
+//     console.log(val);
+// });
+// let userNames: string[] = ["Ashar", "Zunno", "Saima", "Moosa", "Arham"];
+// let upperCaseNames = userNames.map((val)=>{
+//     console.log(val);
+//     return val.toUpperCase();
+// })
+// upperCaseNames.forEach((val)=>{
+//     console.log(`UpperCase Name: ${val}`);
+// }) 
+var userDetails = {
+    name: "Owais Ahmed",
+    age: 23,
+    isValid: true
+};
 console.log(userDetails);
-// null==false  true=1 false=0
-var userId = null;
-if (userId) {
-    console.log("User ID is true");
-}
-else {
-    console.log("User ID is false");
-}
-// undefined==false  true=1 false=0
-var userPost = undefined;
-if (userPost) {
-    console.log("userPost is true");
-}
-else {
-    console.log("userPost is false");
-}
-// Array
-var userMarks = [45, 60, 80, 90];
-userMarks.push(100); //end 100
-userMarks.pop(); //end delete last element
-userMarks.shift(); //start delete
-console.log(userMarks);
+console.log(userDetails.name);
+var carDetails = {
+    name: "BMW",
+    model: "X5",
+    year: 2023,
+    isValid: true
+};
+console.log(carDetails);
+var student = {
+    name: "Owais Ahmed",
+    stdMarks: 90,
+    isActive: true
+};
+console.log(student);
+var mixed;
+mixed = ["Owais", 23, "Ahmed", 90, "Pakistan", 100];
+mixed.forEach(function (val) {
+    if (typeof val === "string") {
+        console.log("String Upper Case: ".concat(val.toUpperCase()));
+    }
+    else {
+        console.log("Number: ".concat(val.toFixed(2)));
+    }
+});
+var stdDetail = {
+    name: "Saima Khalid",
+    age: 17,
+    isActive: false
+};
+console.log(stdDetail.name);
--- TypeScript/basics/app.ts
+++ TypeScript/basics/app.ts
@@ -2,44 +2,117 @@ console.log("Welcome in Typescript");
 
 
 
-let userName: string = "Owais Ahmed";
-let userAge: number = 23;
-let userEmail: string = "owais@gmail.com"
-let isActive: boolean = true;
+// let userName: string = "Owais Ahmed";
+// let userAge: number = 23;
+// let userEmail: string = "owais@gmail.com"
+// let isActive: boolean = true;
 
-console.log(`Hello ${userName}`);
+// console.log(`Hello ${userName}`);
 
 
-let userDetails = `User Name: ${userName}, User Age: ${userAge}, User Email: ${userEmail}, Is Active: ${isActive}`;
+// let userDetails = `User Name: ${userName}, User Age: ${userAge}, User Email: ${userEmail}, Is Active: ${isActive}`;
+// console.log(userDetails);
+
+// // null==false  true=1 false=0
+
+// let userId = null;
+
+// if(userId){
+//     console.log("User ID is true");
+// }else{
+//     console.log("User ID is false");
+// }
+
+// // undefined==false  true=1 false=0
+
+// let userPost = undefined;
+
+// if(userPost){
+//     console.log("userPost is true");
+// }else{
+//     console.log("userPost is false");
+// }
+
+// // Array
+
+// let userMarks: number[] = [45, 60, 80, 90];
+// // userMarks.push(100); //end 100
+// // userMarks.pop(); //end delete last element
+// // userMarks.shift(); //start delete
+// console.log(userMarks);
+
+// // forEach loop
+
+// userMarks.forEach((val)=>{
+//     console.log(val);
+// });
+
+
+// let userNames: string[] = ["Ashar", "Zunno", "Saima", "Moosa", "Arham"];
+
+// let upperCaseNames = userNames.map((val)=>{
+//     console.log(val);
+//     return val.toUpperCase();
+// })
+
+// upperCaseNames.forEach((val)=>{
+//     console.log(`UpperCase Name: ${val}`);
+// }) 
+
+let userDetails: any = {
+    name: "Owais Ahmed",
+    age: 23,
+    isValid: true
+}
+
+
 console.log(userDetails);
+console.log(userDetails.name);
 
-// null==false  true=1 false=0
 
-let userId = null;
-
-if(userId){
-    console.log("User ID is true");
-}else{
-    console.log("User ID is false");
+let carDetails: object = {
+    name: "BMW",
+    model: "X5",
+    year: 2023,
+    isValid: true
 }
 
-// undefined==false  true=1 false=0
+console.log(carDetails);
 
-let userPost = undefined;
-
-if(userPost){
-    console.log("userPost is true");
-}else{
-    console.log("userPost is false");
+let student: {
+    name: string,
+    stdMarks: number,
+    isActive: boolean,
+} = {
+    name: "Owais Ahmed",
+    stdMarks: 90,
+    isActive: true
 }
 
-// Array
+console.log(student);
 
-let userMarks: number[] = [45, 60, 80, 90];
-userMarks.push(100); //end 100
-userMarks.pop(); //end delete last element
-userMarks.shift(); //start delete
-console.log(userMarks);
+let mixed: (string | number) [];
+mixed = ["Owais", 23, "Ahmed", 90, "Pakistan", 100];
 
+mixed.forEach((val)=>{
+    if(typeof val === "string"){
+        console.log(`String Upper Case: ${val.toUpperCase()}`)
+    }else{
+        console.log(`Number: ${val.toFixed(2)}`)    
+    }
+})
 
+//Aliased Object Type
+type Std = {
+    name: string,
+    age: number,
+    isActive: boolean
+}
 
+let stdDetail: Std = {
+    name: "Saima Khalid",
+    age: 17,
+    isActive: false
+}
+
+console.log(stdDetail.name);
