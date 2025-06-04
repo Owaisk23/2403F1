e4a72dac3c476b7b33938c9d50b19e532b5a650d
owaisk23 <owais1566@gmail.com>
2025-06-04 18:54:33 +0500

    added order by, aggregate funcs

--- SQL Server/basics.sql
+++ SQL Server/basics.sql
@@ -35,4 +35,66 @@ UPDATE Employee set empName = 'Burney', city = 'Lhr' where id = 5;
 
 DELETE FROM Employee where id = 5;
 
+-- where clause
+
 SELECT empName, designation FROM Employee where designation = 'Angular Developer';
+
+--not operator
+SELECT empName, designation FROM Employee where not designation = 'Angular Developer';
+
+-- AND operator
+
+SELECT empName, designation, salary FROM Employee where not designation = 'Angular Developer'
+AND salary > 150000;
+
+
+SELECT empName, designation, salary FROM Employee where not designation = 'Angular Developer'
+AND salary < 150000;
+
+-- OR Operator
+
+SELECT empName, designation, salary FROM Employee where not designation = 'Angular Developer'
+OR salary > 150000;
+
+SELECT empName, designation, salary FROM Employee where not designation = 'Angular Developer'
+OR salary < 150000;
+
+-- between
+SELECT * FROM Employee where id between 2 and 6;
+
+-- like 
+SELECT city FROM Employee where city like '%kh%';
+SELECT * FROM Employee where city like '%i%' AND designation like '%Ang%';
+
+-- IN
+
+SELECT * FROM Employee where city IN ('Khi');
+
+-- Order By
+
+SELECT * FROM Employee order by empName asc;
+
+SELECT * FROM Employee order by salary desc;
+
+-- top
+
+select top 4 * from Employee;
+
+select top 50 percent * from Employee order by empName asc;
+
+-- aggregate functions
+
+SELECT COUNT(empName) as total_cities FROM Employee;
+
+SELECT MIN(salary) as min_salary FROM Employee;
+
+SELECT MAX(salary) as max_salary FROM Employee;
+
+SELECT SUM(salary) as totalsum_salary FROM Employee;
+
+SELECT AVG(salary) as avg_salary FROM Employee;
+
+SELECT CONCAT(empName , ' has a designation of ', designation, ' has salary of ', salary)
+as emp_details FROM Employee;
+
+
