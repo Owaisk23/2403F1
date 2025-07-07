0c391fd5fba77e99b364853c930da259cfe179f6
owaisk23 <owais1566@gmail.com>
2025-07-07 18:54:48 +0500

    added baiscs of csharp

--- CSharp/FirstApplication/FirstApplication/Program.cs
+++ CSharp/FirstApplication/FirstApplication/Program.cs
@@ -24,25 +24,25 @@ decimal deciNum = 770012.1562M;
 
 bool isValid = false;
 
-Console.WriteLine(userName + " " + "age: " + age + " lucky number: " + number + " char is: " + alpha + " has height: " + height + " has valid ID: " + isValid);
+//Console.WriteLine(userName + " " + "age: " + age + " lucky number: " + number + " char is: " + alpha + " has height: " + height + " has valid ID: " + isValid);
 
 //Arithematic Operators
 
-Console.WriteLine(90 + 30);
-Console.WriteLine(90 - 30);
-Console.WriteLine(90 * 30);
-Console.WriteLine(90 / 30);
-Console.WriteLine(90 % 30);
+//Console.WriteLine(90 + 30);
+//Console.WriteLine(90 - 30);
+//Console.WriteLine(90 * 30);
+//Console.WriteLine(90 / 30);
+//Console.WriteLine(90 % 30);
 
 //Assignment Operator
 
-int num = 11;
+//int num = 11;
 
-Console.WriteLine(num += 4); //num = num + 4
-Console.WriteLine(num -= 4); //num = num - 4
-Console.WriteLine(num *= 4); //num = num * 4
-Console.WriteLine(num /= 4); //num = num / 4
-Console.WriteLine(num %= 4); //num = num % 4
+//Console.WriteLine(num += 4); //num = num + 4
+//Console.WriteLine(num -= 4); //num = num - 4
+//Console.WriteLine(num *= 4); //num = num * 4
+//Console.WriteLine(num /= 4); //num = num / 4
+//Console.WriteLine(num %= 4); //num = num % 4
 
 //Comparison Operators (True/ False)
 
@@ -62,8 +62,8 @@ int a = 45; int b = 70;
 //F  T = F
 //F  F = F
 
-Console.WriteLine(a <= b && a != b); //TRUE
-Console.WriteLine(a >= b && a != b); //FALSE
+//Console.WriteLine(a <= b && a != b); //TRUE
+//Console.WriteLine(a >= b && a != b); //FALSE
 
 //OR
 //T  T = T
@@ -71,9 +71,66 @@ Console.WriteLine(a >= b && a != b); //FALSE
 //F  T = T
 //F  F = F
 
-Console.WriteLine(a <= b || a != b); //TRUE
-Console.WriteLine(a >= b || a != b); //TRUE
-Console.WriteLine(a >= b || a == b); //FALSE
+//Console.WriteLine(a <= b || a != b); //TRUE
+//Console.WriteLine(a >= b || a != b); //TRUE
+//Console.WriteLine(a >= b || a == b); //FALSE
+
+
+// NOT !
+
+//Console.WriteLine(!(a == b)); // FAlSE
+//Console.WriteLine(a != b); // TRUE
+
+// SALARY SYSTEM
+//Console.WriteLine("Enter your salary..");
+
+//int salary = Int32.Parse(Console.ReadLine());
+
+//Console.WriteLine("Your Salary: " + salary);
+
+
+//SWITCH 1 to 7 || 1==Monday   7==Sunday
+
+Console.WriteLine("Enter number between 1 to 7");
+int num = Int32.Parse(Console.ReadLine());
+
+switch (num)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+    case 7:
+        Console.WriteLine("Sunday");
+        break;
+    default:
+        Console.WriteLine("Invalid Number");
+        break;
+}
+
+
+//sting interpolation
+
+int six = 6;
+
+Console.WriteLine($"This is some number: {six * 2}");
+
+
+
 
 
 
--- CSharp/FirstApplication/FirstApplication/obj/Debug/net8.0/FirstApplication.AssemblyInfo.cs
+++ CSharp/FirstApplication/FirstApplication/obj/Debug/net8.0/FirstApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba935e6624aad2bbc0f0f59699f9f14e6ffe0da1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f630b774a7f1f426dbf96a3b8016758b196d99bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
