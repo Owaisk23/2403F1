13022d889b665d15981323af514eaac06700fc45
owaisk23 <owais1566@gmail.com>
2025-07-09 18:58:19 +0500

    added basics

--- CSharp/FirstApplication/FirstApplication/Program.cs
+++ CSharp/FirstApplication/FirstApplication/Program.cs
@@ -82,72 +82,165 @@ int a = 45; int b = 70;
 //Console.WriteLine(a != b); // TRUE
 
 // SALARY SYSTEM
-//Console.WriteLine("Enter your salary..");
+//Console.WriteLine("Enter Salary..");
 
 //int salary = Int32.Parse(Console.ReadLine());
 
-//Console.WriteLine("Your Salary: " + salary);
+//if (salary > 50000 && !(salary >= 80000))
+//{
+//    Console.WriteLine("Good salary...");
+//}
+//else if (salary < 50000)
+//{
+//    Console.WriteLine("Do you get fuel allowance?");
+//    string answer = Console.ReadLine();
+
+//    answer = answer.ToLower();// yes  or y
+
+//    if (answer == "yes" || answer == "y")
+//    {
+//        Console.WriteLine("Sounds Goood!!");
+//    }
+//    else if (answer == "no" || answer == "n")
+//    {
+//        Console.WriteLine("Try to switch your job ASAP!!");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Please enter yes or no.");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Great!! MASHALLAH");
+//}
 
 
 //SWITCH 1 to 7 || 1==Monday   7==Sunday
 
-Console.WriteLine("Enter number between 1 to 7");
-int num = Int32.Parse(Console.ReadLine());
+//Console.WriteLine("Enter number between 1 to 7");
+//int num = Int32.Parse(Console.ReadLine());
 
-switch (num)
-{
-    case 1:
-        Console.WriteLine("Monday");
-        break;
-    case 2:
-        Console.WriteLine("Tuesday");
-        break;
-    case 3:
-        Console.WriteLine("Wednesday");
-        break;
-    case 4:
-        Console.WriteLine("Thursday");
-        break;
-    case 5:
-        Console.WriteLine("Friday");
-        break;
-    case 6:
-        Console.WriteLine("Saturday");
-        break;
-    case 7:
-        Console.WriteLine("Sunday");
-        break;
-    default:
-        Console.WriteLine("Invalid Number");
-        break;
-}
+//switch (num)
+//{
+//    case 1:
+//        Console.WriteLine("Monday");
+//        break;
+//    case 2:
+//        Console.WriteLine("Tuesday");
+//        break;
+//    case 3:
+//        Console.WriteLine("Wednesday");
+//        break;
+//    case 4:
+//        Console.WriteLine("Thursday");
+//        break;
+//    case 5:
+//        Console.WriteLine("Friday");
+//        break;
+//    case 6:
+//        Console.WriteLine("Saturday");
+//        break;
+//    case 7:
+//        Console.WriteLine("Sunday");
+//        break;
+//    default:
+//        Console.WriteLine("Invalid Number");
+//        break;
+//}
 
 
 //sting interpolation
 
-int six = 6;
+//int six = 6;
 
-Console.WriteLine($"This is some number: {six * 2}");
-
-
+//Console.WriteLine($"This is some number: {six * 2}");
 
 
+//Loops
+//for loop
+
+//for(int i = 0; i <= 70; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+//for(int i = 50; i >= 0; i--)
+//{
+//    Console.WriteLine(i);
+//}
+
+//Odd Numbers
+//for(int i = 1; i <= 101; i++)
+//{
+//    if(i%2 != 0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+//PRIME NUMBER
 
 
 
+//WHILE Loop
+
+//int k = 23;
+
+//while(k < 23)
+//{
+//    Console.WriteLine(k);
+//    k++;
+//}
 
 
+//Do- While
+
+//int j = 10;
+//do
+//{
+//    Console.WriteLine("J");
+//    j++;
+//} while (j < 23);
 
 
+//int num;
+
+//char ans;
+
+//do
+//{
+//    Console.WriteLine("Enter any number");
+//    num = Convert.ToInt32(Console.ReadLine());
+
+//    if (num % 2 == 0)
+//    {
+//        Console.WriteLine("Even Number");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Odd Number");
+//    }
+
+//    Console.WriteLine("Do you want to continue?? Press Y");
+//    ans = Convert.ToChar(Console.ReadLine());
+//} while (ans == 'Y' || ans == 'y');
 
 
+// ARRAY FIXED SIZED
+
+int[] temperatures = new int[5];
+//                          length
+
+temperatures[0] = 43;
+temperatures[1] = 40;
+temperatures[2] = 13;
+temperatures[3] = 23;
+temperatures[4] = 33;
+//temperatures[5] = 60;
 
 
-
-
-
-
-
+Console.WriteLine(temperatures[4]);
 
 
 
--- CSharp/FirstApplication/FirstApplication/obj/Debug/net8.0/FirstApplication.AssemblyInfo.cs
+++ CSharp/FirstApplication/FirstApplication/obj/Debug/net8.0/FirstApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f630b774a7f1f426dbf96a3b8016758b196d99bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c391fd5fba77e99b364853c930da259cfe179f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
