51eb1bb69b28a600654b8355b53fac1abebe6058
owaisk23 <owais1566@gmail.com>
2025-10-03 18:14:21 +0500

    added Temp Embedded project in dotnet

--- DotNet/CookieAuthentication/CookieAuthentication/obj/Debug/net8.0/CookieAuthentication.AssemblyInfo.cs
+++ DotNet/CookieAuthentication/CookieAuthentication/obj/Debug/net8.0/CookieAuthentication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookieAuthentication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2868310e871391f5b127ecd0dc4e69dde4c8c55c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookieAuthentication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookieAuthentication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
