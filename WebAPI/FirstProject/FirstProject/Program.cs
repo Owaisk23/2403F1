d647ba1e304cfc07aa21f69cf5573571084fd8d6
owaisk23 <owais1566@gmail.com>
2025-11-10 18:27:01 +0500

    added cors in backend

--- WebAPI/FirstProject/FirstProject/Program.cs
+++ WebAPI/FirstProject/FirstProject/Program.cs
@@ -3,6 +3,18 @@ using FirstProject.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200") // Add your MVC URL
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
