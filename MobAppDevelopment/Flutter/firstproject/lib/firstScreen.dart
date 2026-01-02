627d4b8dfad49345b0c9b2326fa8cb9ac591849f
owaisk23 <owais1566@gmail.com>
2026-01-02 18:56:08 +0500

    added single child scroll view

--- MobAppDevelopment/Flutter/firstproject/lib/firstScreen.dart
+++ MobAppDevelopment/Flutter/firstproject/lib/firstScreen.dart
@@ -24,96 +24,226 @@ class _FirstScreenState extends State<FirstScreen> {
         foregroundColor: Colors.white,
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Text(
-              "Welcome to First Screen",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w500,
-                fontFamily: 'Verdana',
+      body: Padding(
+        // padding: const EdgeInsets.all(28.0),
+        // padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+        padding: const EdgeInsets.only(top: 25.0, bottom: 15.0, left: 30.0, right: 10.0),
+        child: Center(
+          child: ListView(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "Welcome to First Screen",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'Verdana',
+                ),
               ),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.blue,
-              child: Icon(Icons.home, color: Colors.white, size: 50),
-            ),
-            SizedBox(height: 10),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.red,
-              child: Icon(Icons.phone, color: Colors.white, size: 50),
-            ),
-            SizedBox(height: 10),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.yellow,
-              child: Icon(Icons.message, color: Colors.white, size: 50),
-            ),
-            SizedBox(height: 10),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.brown,
-              child: Icon(Icons.book, color: Colors.white, size: 50),
-            ),
-            SizedBox(height: 10),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.green,
-              child: Icon(
-                Icons.notification_add,
-                color: Colors.white,
-                size: 50,
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.blue,
+                child: Icon(Icons.home, color: Colors.white, size: 50),
               ),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.blue,
-              child: Icon(Icons.home, color: Colors.white, size: 50),
-            ),
-            SizedBox(height: 10),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.red,
-              child: Icon(Icons.phone, color: Colors.white, size: 50),
-            ),
-            SizedBox(height: 10),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.yellow,
-              child: Icon(Icons.message, color: Colors.white, size: 50),
-            ),
-            SizedBox(height: 10),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.brown,
-              child: Icon(Icons.book, color: Colors.white, size: 50),
-            ),
-            SizedBox(height: 10),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.green,
-              child: Icon(
-                Icons.notification_add,
-                color: Colors.white,
-                size: 50,
+              SizedBox(height: 10),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+                child: Icon(Icons.phone, color: Colors.white, size: 50),
               ),
-            ),
-          ],
+              SizedBox(height: 10),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.yellow,
+                child: Icon(Icons.message, color: Colors.white, size: 50),
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.brown,
+                child: Icon(Icons.book, color: Colors.white, size: 50),
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.green,
+                child: Icon(
+                  Icons.notification_add,
+                  color: Colors.white,
+                  size: 50,
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.blue,
+                child: Icon(Icons.home, color: Colors.white, size: 50),
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+                child: Icon(Icons.phone, color: Colors.white, size: 50),
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.yellow,
+                child: Icon(Icons.message, color: Colors.white, size: 50),
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.brown,
+                child: Icon(Icons.book, color: Colors.white, size: 50),
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.green,
+                child: Icon(
+                  Icons.notification_add,
+                  color: Colors.white,
+                  size: 50,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+
+
+
+
+      // body: SingleChildScrollView(
+      //   scrollDirection: Axis.vertical,
+      //   child: Center(
+      //     child: Column(
+      //       // mainAxisAlignment: MainAxisAlignment.start,
+      //       children: [
+      //         Text(
+      //           "Welcome to First Screen",
+      //           style: TextStyle(
+      //             fontSize: 24,
+      //             fontWeight: FontWeight.w500,
+      //             fontFamily: 'Verdana',
+      //           ),
+      //         ),
+      //         Container(
+      //           height: 100,
+      //           width: 100,
+      //           color: Colors.blue,
+      //           child: Icon(Icons.home, color: Colors.white, size: 50),
+      //         ),
+      //         SizedBox(height: 10),
+      //         Container(
+      //           height: 100,
+      //           width: 100,
+      //           color: Colors.red,
+      //           child: Icon(Icons.phone, color: Colors.white, size: 50),
+      //         ),
+      //         SizedBox(height: 10),
+      //         Container(
+      //           height: 100,
+      //           width: 100,
+      //           color: Colors.yellow,
+      //           child: Icon(Icons.message, color: Colors.white, size: 50),
+      //         ),
+      //         SizedBox(height: 10),
+      //         Container(
+      //           height: 100,
+      //           width: 100,
+      //           color: Colors.brown,
+      //           child: Icon(Icons.book, color: Colors.white, size: 50),
+      //         ),
+      //         SizedBox(height: 10),
+      //         Container(
+      //           height: 100,
+      //           width: 100,
+      //           color: Colors.green,
+      //           child: Icon(
+      //             Icons.notification_add,
+      //             color: Colors.white,
+      //             size: 50,
+      //           ),
+      //         ),
+      //         Container(
+      //           height: 100,
+      //           width: 100,
+      //           color: Colors.blue,
+      //           child: Icon(Icons.home, color: Colors.white, size: 50),
+      //         ),
+      //         SizedBox(height: 10),
+      //         Container(
+      //           height: 100,
+      //           width: 100,
+      //           color: Colors.red,
+      //           child: Icon(Icons.phone, color: Colors.white, size: 50),
+      //         ),
+      //         SizedBox(height: 10),
+      //         Container(
+      //           height: 100,
+      //           width: 100,
+      //           color: Colors.yellow,
+      //           child: Icon(Icons.message, color: Colors.white, size: 50),
+      //         ),
+      //         SizedBox(height: 10),
+      //         Container(
+      //           height: 100,
+      //           width: 100,
+      //           color: Colors.brown,
+      //           child: Icon(Icons.book, color: Colors.white, size: 50),
+      //         ),
+      //         SizedBox(height: 10),
+      //         Container(
+      //           height: 100,
+      //           width: 100,
+      //           color: Colors.green,
+      //           child: Icon(
+      //             Icons.notification_add,
+      //             color: Colors.white,
+      //             size: 50,
+      //           ),
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
