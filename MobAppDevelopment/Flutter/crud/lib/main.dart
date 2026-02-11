a55a10382388bce13028e8996ee57f6628b7e606
owaisk23 <owais1566@gmail.com>
2026-02-11 18:53:10 +0500

    added add product

---
+++ MobAppDevelopment/Flutter/crud/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:crud/Screens/addproduct.dart';
+import 'package:crud/Screens/products.dart';
+import 'package:crud/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main()async {
+
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+   runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Products(),
+    ),
+  );
+}
